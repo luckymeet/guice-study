c35261101da950046541795deba205062c7f1b25
yuminjun <yuminjun@lexiangbao.com>
2020-09-25 14:59:56 +0800

    添加SqlSessionTemplate，在方法调用时通过sqlSession对事物进行管理

--- src/main/java/com/study/module/PublicMoudle.java
+++ src/main/java/com/study/module/PublicMoudle.java
@@ -2,6 +2,7 @@ package com.study.module;
 
 import com.google.inject.AbstractModule;
 import com.study.common.mybatis.MybatisManager;
+import com.study.common.mybatis.SqlSessionTemplate;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 
@@ -10,12 +11,18 @@ import java.util.Set;
 public class PublicMoudle extends AbstractModule {
     @Override
     protected void configure() {
+        mapperConfig();
+    }
+
+    private void mapperConfig() {
         MybatisManager mybatisManager = new MybatisManager();
         SqlSessionFactory sqlSessionFactory = mybatisManager.createSqlSessionFactory();
-        SqlSession sqlSession = sqlSessionFactory.openSession();
+        SqlSessionTemplate sqlSessionTemplate = new SqlSessionTemplate(sqlSessionFactory);
         Set<Class<? extends Class<?>>> mapperClassSet = mybatisManager.getMapperClassSet();
         for (Class clazz : mapperClassSet) {
-            bind(clazz).toInstance(sqlSession.getMapper(clazz));
+            bind(clazz).toInstance(sqlSessionTemplate.getMapper(clazz));
         }
+        bind(SqlSessionFactory.class).toInstance(sqlSessionFactory);
+        bind(SqlSession.class).toInstance(sqlSessionTemplate);
     }
 }
